83401a9d05361a4db0d4129ec6a16da2a0ccea0f
Younisprogramming <younistaher12341@gmail.com>
2024-08-13 19:26:48 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
 """
-Script that queries subscribers on a given Reddit subreddit.
+number of subscribers for a given subreddit
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    # Define the URL of the subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Set custom User-Agent to avoid "Too Many Requests" error
+    headers = {"User-Agent": "custom"}
+
+    try:
+
+        response = requests.get(url, headers=headers, allow_redirects=False)
+
+        if response.status_code == 200:
+            data = response.json()
+            return data.get("data", {}).get("subscribers", 0)
+        else:
+            # If the response status code is not 200, return 0
+            return 0
+    except Exception as e:
+        # If an exception occurs (e.g., network error), return 0
+        print(f"Error: {e}")
         return 0
